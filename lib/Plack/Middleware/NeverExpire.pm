daabee825740907b866fb87b8e16811ee8e44b27
Αριστοτέλης Παγκαλτζής <pagaltzis@gmx.de>
2015-01-04 06:34:30 +0000

    Import of ARISTOTLE/Plack-Middleware-NeverExpire-1.005.tar.gz from CPAN.
    gitpan-cpan-distribution: Plack-Middleware-NeverExpire
    gitpan-cpan-version:      1.005
    gitpan-cpan-path:         ARISTOTLE/Plack-Middleware-NeverExpire-1.005.tar.gz
    gitpan-cpan-author:       ARISTOTLE
    gitpan-cpan-maturity:     released

--- lib/Plack/Middleware/NeverExpire.pm
+++ lib/Plack/Middleware/NeverExpire.pm
@@ -1,24 +1,25 @@
-package Plack::Middleware::NeverExpire;
-{
-  $Plack::Middleware::NeverExpire::VERSION = '1.004';
-}
+use 5.008001;
 use strict;
-use parent 'Plack::Middleware';
+use warnings;
 
+package Plack::Middleware::NeverExpire;
+$Plack::Middleware::NeverExpire::VERSION = '1.005';
 # ABSTRACT: set expiration headers far in the future
+
+use parent 'Plack::Middleware';
 
 use Plack::Util ();
 use Time::Piece ();
-use Time::Seconds 'ONE_YEAR';
+use Time::Seconds ();
 
 sub call {
 	my $self = shift;
 	Plack::Util::response_cb( $self->app->( shift ), sub {
 		my $res = shift;
 		return if $res->[0] != 200;
-		my $date = Time::Piece->gmtime( time + ONE_YEAR );
+		my $date = Time::Piece->gmtime( time + Time::Seconds::ONE_YEAR );
 		Plack::Util::header_set( $res->[1], 'Expires', $date->strftime );
-		Plack::Util::header_push( $res->[1], 'Cache-Control', 'max-age=' . ONE_YEAR . ', public' );
+		Plack::Util::header_push( $res->[1], 'Cache-Control', 'max-age=' . Time::Seconds::ONE_YEAR . ', public' );
 		return;
 	} );
 }
@@ -29,13 +30,15 @@ __END__
 
 =pod
 
+=encoding UTF-8
+
 =head1 NAME
 
 Plack::Middleware::NeverExpire - set expiration headers far in the future
 
 =head1 VERSION
 
-version 1.004
+version 1.005
 
 =head1 SYNOPSIS
 
@@ -78,7 +81,7 @@ Aristotle Pagaltzis <pagaltzis@gmx.de>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2013 by Aristotle Pagaltzis.
+This software is copyright (c) 2015 by Aristotle Pagaltzis.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
